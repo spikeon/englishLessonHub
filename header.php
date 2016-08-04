225c517fffa117782a0765b11a3e202746420927
Michael Flynn <mflynn@cngann.com>
2016-08-03 20:10:41 -0700

    removing integrety

--- header.php
+++ header.php
@@ -13,12 +13,12 @@
 	<?php /* <script src="//code.jquery.com/jquery-1.11.3.min.js"></script> */ ?>
 	<script src="node_modules/jquery/dist/jquery.min.js"></script>
 
-	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-theme.min.css">
 
 	<link rel="stylesheet" href="style.css">
 
-	<script src="node_modules/bootstrap/dist/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 
 	<?php if(login_type() != 'admin'){ ?>
 		<script src="node_modules/ckeditor/ckeditor.js"></script>
