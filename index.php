b7887c2b29e533386b5b813521db9afc43e939ba
Michael Flynn <mflynn@cngann.com>
2016-08-03 22:19:30 -0700

    cleanup of array data pushed to home page, cleanup of php for home page

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<?php include('header.php'); ?>
-<script>
-	var teachers = <?php
+<?php 
+	include('header.php');
 	$teachers = [];
 	foreach ($db->query('SELECT id FROM teacher WHERE ban = 0') as $row) {
 		$teacher = get_teacher($row['id']);
-
 
 		if(login_type() == 'student'){
 			$failed = false;
@@ -16,13 +14,13 @@
 			if ($student->free_classes >= $billing_info['maxFreeClasses']) $failed = true;
 			if($failed) $teacher->freeclass = 0;
 		}
+		$teacher->password = "";
+		$teacher->email = "";
+		$teacher->skype = "";
+		$teacher->paypal = "";
 
 		$teachers[] = $teacher;
 	}
-	echo json_encode($teachers);
-	?>;
-</script>
-
-<div ng-app="home"><home-page></home-page></div>
-
-<?php include('footer.php'); ?>
+	echo '<script> var teachers = ' . json_encode($teachers) . ';</script>';
+	echo '<div ng-app="home"><home-page></home-page></div>';
+	include('footer.php');
