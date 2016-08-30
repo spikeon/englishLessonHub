13ca881c12297d7db2eab9cf9ebf45ec3106a5b4
Michael Flynn <mflynn@cngann.com>
2016-08-29 23:12:32 -0700

    availability form

--- availability.js
+++ availability.js
@@ -6,13 +6,18 @@ $(function(){
 		$admin.find('.removeavail').click(function(){
 			$(this).parents('.row').first().remove();
 		});
+
 		$admin.find('.newavail').click(function(){
 			var $new = $admin.find('.availtemplate').clone(true,true).show().removeClass('availtemplate');
+			$new.clonePolyfill();
+			$new.find('.has-error').removeClass('has-error');
+			$new.find('.has-success').removeClass('has-success');
 			availctr++;
 			$new.find('.day').attr('name', 'new['+availctr+'][day]');
 			$new.find('.start').attr('name', 'new['+availctr+'][start]');
 			$new.find('.end').attr('name', 'new['+availctr+'][end]');
 			$admin.append($new);
+			$new.updatePolyfill();
 		});
 
 	});
